65c6f1bbaf951962e5ed1d0e2a7201d0011eaa64
Brandon <brandon.cole@bison.howard.edu>
2018-08-25 16:45:43 -0400

    added extern to functions and string library header

--- list.h
+++ list.h
@@ -1,5 +1,6 @@
 #include <inttypes.h>
 #include <stdbool.h>
+#include <string.h>
 
 extern struct CharNode;
 
@@ -18,21 +19,21 @@ extern int add_to_list(struct CharNode*, char*);
 /* Removes the string from the front of the list and
 * returns a pointer to it. The caller is expected to free
 * the string returned when finished with it. */
-char* remove_from_list(struct CharNode*);
+extern char* remove_from_list(struct CharNode*);
 
 /* Prints every string in the list, with a new line
 * character at the end of each string */
-void print_list(struct CharNode*);
+extern void print_list(struct CharNode*);
 
 
 /* Flushes (clears) the entire list and re-initializes the
 * list. The passed pointer ll should still point to a
 * valid, empty list when this function returns. Any memory
 * allocated to store items in the list should be freed. */
-void flush_list(struct CharNode*);
+extern void flush_list(struct CharNode*);
 
 
 /* De-allocates all data for the list. Ensure all memory
 * allocated for this list is freed, including any
 * allocated strings and the list itself. */
-void free_list(struct CharNode*);
+extern void free_list(struct CharNode*);
