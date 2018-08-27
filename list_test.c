5e1d9b539a6fdcc728819f0fa0b3c59711322976
Bcoding21 <brandoncole673@gmail.com>
2018-08-27 14:59:48 -0400

    Add files via upload

--- list_test.c
+++ list_test.c
@@ -1,50 +1,45 @@
-#include "list_test.h"
+#include "list.h"
 
-bool test_create_list(){
-    struct Node* newList = create_list();
-	bool didCreate = newList != NULL;
-	if (didCreate){
-		free(newList);
-	}
-	return didCreate;
+int main(){
+
+	puts("Created list");
+	struct Node* list_head = create_list();
+	puts("Printing list");
+	print_list(list_head);
+
+	puts("Adding");
+	add_to_list(head, "this");
+	print_list(head);
+	add_to_list(head, "is");
+	print_list(head);
+	add_to_list(head, "test");
+	print_list(head);
+
+
+	puts("Removing");
+	remove_from_list(head);
+	print_list(head);
+	remove_from_list(head);
+	print_list(head);
+	remove_from_list(head);
+	print_list(head);
+
+
+	puts("Adding");
+	add_to_list(head, "this");
+	print_list(head);
+	add_to_list(head, "is");
+	print_list(head);
+	add_to_list(head, "test");
+	print_list(head);
+
+	puts("Flushing");
+	flush_list(head);
+	print_list(head);
+
+	puts("Freeing");
+	free_list(head);
+	print_list(head);
+
 }
-
-bool test_add_to_list(){
-	char* str = "test";
-	struct Node* node = malloc(sizeof(struct Node));
-	node->str = str;
-	node->next = NULL;
-	add_to_list(node, str);
-	bool nodeAdded = strcmp(node->str, str) == 0;
-	free(node->str);
-	free(node);
-	return nodeAdded;
-}
-
-bool test_remove_from_list(){
-	char* str = "test";
-	struct Node* head_node = malloc(sizeof(struct Node));
-	head_node->str = str;
-	head_node->next = NULL;
-	char* str2 = remove_from_list(head_node);
-	return strcmp(str2, head_node->str) == 0;
-}
-
-bool test_flush_list() {
-	struct Node* head_node = malloc(sizeof(struct Node));
-	head_node->str = strdup("test");
-	head_node->next = NULL;
-	flush_list(head_node);
-	return head_node->str == NULL;
-}
-
-bool test_free_list(){
-	char* str = "test";
-	struct Node* head_node = malloc(sizeof(struct Node));
-	head_node->str = strdup("test");
-	head_node->next = NULL;
-	free_list(&head_node);
-	return head_node == NULL;
-}
-
 
--- makefile
+++ makefile
@@ -1,2 +1,2 @@
-list_test3: list_test.c list.c test.c
-	gcc -o run_test list_test.c list.c test.c -I.
+list_test3: list_test.c list.c 
+	gcc -o run_test list_test.c list.c -I.
